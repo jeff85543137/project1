77d878608f5fcd8e633f2b915d097874c5107014
jeff <120872344@qq.com>
2018-11-05 21:48:29 +0800

    aa

--- src/main/java/cn/bj/service/MobileService.java
+++ src/main/java/cn/bj/service/MobileService.java
@@ -1,6 +1,7 @@
 package cn.bj.service;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,7 +12,7 @@ import cn.bj.pojo.Mobile;
 public interface MobileService {
 
 
-	@GET
+	@POST
 	@Path("/get/{mobilenumber}")
 	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
 	public Mobile getMobile(@PathParam("mobilenumber")String mobilenumber);
